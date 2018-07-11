6cc4c63217f16e8004125c608ff22692aceb1fd5
Denis Mehmed <demehmed@everis.com>
2018-07-11 13:13:10 +0200

    Forgot to upload app.component test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
       providers: [FeedService]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
